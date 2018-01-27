313650a9a97798d553be7ee9f697a13b9acb1c1f
Anh Vu <vu.haianh291@gmail.com>
2018-01-27 21:52:15 +0200

    Change color filter

--- src/app/color-filter/color-filter.component.ts
+++ src/app/color-filter/color-filter.component.ts
@@ -21,11 +21,11 @@ export class ColorFilterComponent implements OnInit {
     // change brighness and contrast
     for (let i = 0; i < this.imageService.numPixels; i++) {
       this.imageService.pixels[i * 4] = this.imageService.pixels[i * 4] +
-         this.red / (101 - this.strength);
+         this.red % (101 - this.strength);
       this.imageService.pixels[i * 4 + 1] = this.imageService.pixels[i * 4 + 1] +
-      this.green / (101 - this.strength);
+      this.green % (101 - this.strength);
       this.imageService.pixels[i * 4 + 2] = this.imageService.pixels[i * 4 + 2] +
-        this.blue / (101 - this.strength);
+        this.blue % (101 - this.strength);
     }
 
     this.imageService.context.clearRect(0, 0, this.imageService.canvas.width, this.imageService.canvas.height);
